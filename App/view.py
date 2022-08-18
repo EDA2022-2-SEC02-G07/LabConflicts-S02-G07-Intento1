489273a8bf6e1ad47db4db0f5dc07fe38a39397b
nruizp2 <nruizp2@uniandes.edu.co>
2022-08-18 08:58:14 -0500

    Cambios Lab2 Est1

--- App/view.py
+++ App/view.py
@@ -50,7 +50,7 @@ def printMenu():
     print("1- Cargar Libros")
     print("2- Cargar Tags")
     # TODO: Modificaciones de Est-1 en el Lab 2, agregar opcion 3
-
+    print("3- Cargar Tags de Libros")
     print("0- Salir")
 
 
@@ -76,16 +76,17 @@ def loadBooksTags(control):
     """
     Cargar los Tags de libros
     """
-    # TODO: Modificaciones de Est-1 en el Lab 2
-    pass
+    booktags = controller.loadBooksTags(control,
+        "GoodReads/bppk_tags-small.csv") 
+    return booktags
 
 
 def firstBook(control):
     """
     Devuelve el primer libro del catalogo
     """
-    # TODO: Modificaciones de Est-1 en el Lab 2
-    pass
+    first = firstBook(control)
+    print("Primer libro cargado:\n" + str(first) +  "\n")
 
 
 def lastBook(control):
